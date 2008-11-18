96e82cfab8fab0ca1eaaa239df4e256e7407a942
tschmidt <devteam@dev-shiny.corp.tripractix.com>
2008-11-18 11:28:19 -0500

    Updated the README

---
+++ generators/nifty_cal/nifty_cal_generator.rb
@@ -0,0 +1,11 @@
+class NiftyCalGenerator < Rails::Generator::Base
+  def manifest
+    record do |m|
+      
+      # Add a stylesheet directory for NiftyCal
+      styel_dir = "public/stylesheets/nifty_cal"
+      m.directory style_dir
+      m.file File.join('modern.css'), File.join(style_dir, "modern.css")
+    end
+  end
+end
